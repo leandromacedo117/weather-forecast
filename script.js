903c0e08c27eabbaaa0f7a0138e021536ead0eab
Leandro Macedo <leeandromacedo117@gmail.com>
2023-12-11 18:57:47 -0300

    script

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//39c024bfe56ddf1b82e0722f945b852b
+const apiKey = "39c024bfe56ddf1b82e0722f945b852b";
+// const digla = digla
+const apiCountryURL = "https://flagsapi.com/BR/flat/32.png"
+
+const inputCity = document.getElementById("input-city")
+const searchButton = document.getElementById("search-button")
+
+// Elementos dinamicos
+const cityElement = document.getElementById("city")
+const weatherIcon = document.getElementById("weather-icon")
+const countryElement = document.getElementById("country-element")
+const umidityElement = document.getElementById("umidity")
+const tempElement = document.querySelector("#temperadura span")
+const description = document.querySelector(".description")
+const windElement = document.querySelector(".wind span")
+
+// function 
+
+const getWeatherData = async(cityValue) =>{
+    
+}
+
+const showWeatherData = (cityValue) =>{
+    
+}
+
+
+// Events
+searchButton.addEventListener("click", (e) =>{
+    e.preventDefault()
+    const cityValue = inputCity.value
+    showWeatherData(cityValue)
+    
+})
