65accdb877c267d088b4f098d70b5a6b28b0e0ca
Leandro Macedo <leeandromacedo117@gmail.com>
2023-12-12 15:05:20 -0300

    editando script

--- script.js
+++ script.js
@@ -10,22 +10,35 @@ const cityElement = document.getElementById("city")
 const weatherIcon = document.getElementById("weather-icon")
 const countryElement = document.getElementById("country-element")
 const umidityElement = document.getElementById("umidity")
-const tempElement = document.querySelector("#temperadura span")
+const tempElement = document.querySelector("#temp")
 const description = document.querySelector(".description")
 const windElement = document.querySelector(".wind span")
 
 // function 
 
-const getWeatherData = async(cityValue) =>{
+const getWeatherData = async (cityValue) =>{
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=metric&appid=${apiKey}&lang=pt_br`
 
     const res = await fetch(apiWeatherURL)
     const data = await res.json()
     console.log(data)
+    return data
 }
 
-const showWeatherData = (cityValue) =>{
-    getWeatherData(cityValue)
+const showWeatherData = async (cityValue) =>{
+    const data =  await getWeatherData(cityValue)
+
+    cityElement.textContent = data.name
+
+    let tempo = parseFloat((data.main.temp))
+    tempElement.innerText = tempo.toFixed(1)
+
+    description.innerText = data.weather[0].description 
+    const icon = data.weather[0].icon
+    weatherIcon.setAttribute("src",`http://openweathermap.org/img/wn/${icon}.png"`)
+    umidityElement.textContent = parseInt(data.main.humidity) + "%"
+    windElement.innerText  = data.wind.speed + "Km/h"
+
 }
 
 
